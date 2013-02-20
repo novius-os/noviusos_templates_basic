8e9681b7ad013065847d45c8270301ef124215f5
Julian Esperat <esperat@novius.fr>
2013-02-20 17:51:28 +0100

    "Importing translations"

--- lang/fr/common.lang.php
+++ lang/fr/common.lang.php
@@ -1,9 +1,24 @@
 <?php
 
+// Generated on 20/02/2013 17:43:50
+
+// 5 out of 5 messages are translated (100%).
+// 18 out of 18 words are translated (100%).
+
 return array(
+    #: config/template.config.php:15
     'My super website' => 'Mon super site',
-    'Footer link' => 'Lien de bas de page',
-    'Another footer link' => 'Un autre lien de bas de page',
-    'Powered by Novius OS' => 'Propulsé par Novius OS',
+
+    #: views/subviews/page_contexts.view.php:24
     'This page also exists in: {{contexts}}' => 'Cette page est également disponible en&nbsp;: {{contexts}}',
+
+    #: views/subviews/footer.view.php:16
+    'Footer link' => 'Lien de bas de page',
+
+    #: views/subviews/footer.view.php:17
+    'Another footer link' => 'Un autre lien de bas de page',
+
+    #: views/subviews/footer.view.php:18
+    'Powered by Novius OS' => 'Propulsé par Novius OS',
+
 );
