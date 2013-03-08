d1d62a94873f6e3c5caea740c3659262db05648c
Sébastien Drouyer <drouyer@novius.com>
2013-03-08 16:12:12 +0100

    migrations

---
+++ migrations/001_version_0_2.sql
@@ -0,0 +1,2 @@
+UPDATE `nos_page` SET `page_template` = 'noviusos_top_menu' WHERE `page_template` = 'top_menu';
+UPDATE `nos_page` SET `page_template` = 'noviusos_left_menu' WHERE `page_template` = 'left_menu';
