adae2e487d97b95acc5ff12c8349f9926512fa22
Julian Esperat <esperat@novius.fr>
2013-04-17 14:54:15 +0200

    "Updating French translations"

--- lang/fr/metadata.lang.php
+++ lang/fr/metadata.lang.php
@@ -1,18 +1,15 @@
 <?php
 
-// Generated on 20/02/2013 17:43:50
+// Generated on 17/04/2013 14:49:10
 
 // 3 out of 3 messages are translated (100%).
 // 12 out of 12 words are translated (100%).
 
 return array(
-    #. Overwritten
     'Novius OS default templates' => 'Gabarits par défaut de Novius OS',
 
-    #. Overwritten
     'Default template (top menu)' => 'Gabarit par défaut (menu en haut)',
 
-    #. Overwritten
     'Default template (left menu)' => 'Gabarit par défaut (menu à gauche)',
 
 );
