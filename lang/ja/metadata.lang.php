b9a7b4162f284402d3810441e5fe7c1ac40f9d26
Julian Esperat <esperat@novius.fr>
2013-04-24 18:02:01 +0200

    "Updating Japanase translations"

---
+++ lang/ja/metadata.lang.php
@@ -0,0 +1,15 @@
+<?php
+
+// Generated on 24/04/2013 17:47:52
+
+// 3 out of 3 messages are translated (100%).
+// 12 out of 12 words are translated (100%).
+
+return array(
+    'Novius OS default templates' => 'Novius OS デフォルトテンプレート',
+
+    'Default template (top menu)' => 'デフォルトテンプレート（上にメニュー）',
+
+    'Default template (left menu)' => 'デフォルトテンプレート（左にメニュー）',
+
+);
