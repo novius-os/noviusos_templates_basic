b6ec01b75b9a8c63bc06531588c8dbf6d7fac9db
Julian Esperat <esperat@novius.fr>
2013-06-25 11:09:06 +0200

    "Updating translations"

--- lang/fr/metadata.lang.php
+++ lang/fr/metadata.lang.php
@@ -1,6 +1,6 @@
 <?php
 
-// Generated on 16/04/2013 19:15:50
+// Generated on 25/06/2013 11:06:52
 
 // 3 out of 3 messages are translated (100%).
 // 12 out of 12 words are translated (100%).
--- lang/ja/common.lang.php
+++ lang/ja/common.lang.php
@@ -1,6 +1,6 @@
 <?php
 
-// Generated on 16/04/2013 19:15:50
+// Generated on 25/06/2013 11:06:52
 
 // 5 out of 5 messages are translated (100%).
 // 18 out of 18 words are translated (100%).
