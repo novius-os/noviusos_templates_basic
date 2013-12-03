d1b9df67dce5bd33dbe2613c9fc91b56c674676b
Gilles FELIX <felix@novius.com>
2013-12-03 09:44:25 +0100

    "Version Chiba 2.3.2"

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -10,7 +10,7 @@
 
 return array(
     'name'    => 'Novius OS default templates',
-    'version' => 'chiba.2.3.1',
+    'version' => 'chiba.2.3.2',
     'provider' => array(
         'name' => 'Novius OS',
     ),
