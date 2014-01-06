4b47fa41d7567e1dbd96cc6df7cafcf25f350921
Gilles FELIX <felix@novius.com>
2014-01-06 15:11:29 +0100

    "Add spanish translation"

---
+++ lang/es/common.lang.php
@@ -0,0 +1,24 @@
+<?php
+
+// Generated on 06/01/2014 15:07:24
+
+// 5 out of 5 messages are translated (100%).
+// 18 out of 18 words are translated (100%).
+
+return array(
+    #: views/subviews/page_contexts.view.php:24
+    'This page also exists in: {{contexts}}' => 'Esta página existe también en {{contexts}}.',
+
+    #: views/subviews/footer.view.php:16
+    'Footer link' => 'Enlace de la parte de abajo de la página',
+
+    #: views/subviews/footer.view.php:17
+    'Another footer link' => 'Otro enlace de la parte de abajo de la página',
+
+    #: views/subviews/footer.view.php:18
+    'Powered by Novius OS' => 'Desarrollado por Novius OS',
+
+    #: config/template.config.php:15
+    'My super website' => 'Mi súper sitio web',
+
+);
---
+++ lang/es/metadata.lang.php
@@ -0,0 +1,15 @@
+<?php
+
+// Generated on 06/01/2014 15:07:24
+
+// 3 out of 3 messages are translated (100%).
+// 12 out of 12 words are translated (100%).
+
+return array(
+    'Novius OS default templates' => 'Plantillas por defecto de Novius OS',
+
+    'Default template (top menu)' => 'Plantilla por defecto (menú de arriba)',
+
+    'Default template (left menu)' => 'Plantilla por defecto (menú de abajo)',
+
+);
