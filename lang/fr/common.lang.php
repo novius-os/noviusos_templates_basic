d2865ff0b31137e7c48afe0eb90ae18c281af84f
Gilles FELIX <felix@novius.com>
2014-06-30 15:00:26 +0200

    "Update french translation"

--- lang/fr/common.lang.php
+++ lang/fr/common.lang.php
@@ -1,11 +1,38 @@
 <?php
 
-// Generated on 03/12/2013 16:22:23
+// Generated on 30/06/2014 14:54:18
 
-// 5 out of 5 messages are translated (100%).
-// 18 out of 18 words are translated (100%).
+// 11 out of 11 messages are translated (100%).
+// 32 out of 32 words are translated (100%).
 
 return array(
+    #: config/template.config.php:15
+    'My super website' => 'Mon super site',
+
+    #: config/variation/noviusos_left_menu.config.php:18
+    #: config/variation/noviusos_top_menu.config.php:18
+    'Configuration' => 'Configuration',
+
+    #: config/variation/noviusos_left_menu.config.php:36
+    #: config/variation/noviusos_top_menu.config.php:36
+    'Menu' => 'Menu',
+
+    #: config/variation/noviusos_left_menu.config.php:43
+    #: config/variation/noviusos_top_menu.config.php:43
+    'No menu selected' => 'Aucun menu sélectionné',
+
+    #: config/variation/noviusos_left_menu.config.php:44
+    #: config/variation/noviusos_top_menu.config.php:44
+    'Pick a menu' => 'Choisir un menu',
+
+    #: config/variation/noviusos_left_menu.config.php:45
+    #: config/variation/noviusos_top_menu.config.php:45
+    'Pick another menu' => 'Choisir un autre menu',
+
+    #: config/variation/noviusos_left_menu.config.php:46
+    #: config/variation/noviusos_top_menu.config.php:46
+    'Un-select this menu' => 'Dé-sélectionné ce menu',
+
     #: views/subviews/page_contexts.view.php:24
     'This page also exists in: {{contexts}}' => 'Cette page est également disponible en&nbsp;: {{contexts}}',
 
@@ -17,8 +44,5 @@ return array(
 
     #: views/subviews/footer.view.php:18
     'Powered by Novius OS' => 'Propulsé par Novius OS',
-
-    #: config/template.config.php:15
-    'My super website' => 'Mon super site',
 
 );
